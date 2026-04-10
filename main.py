c6993b44fa3f81337702244bc671c6d55b7ce2d9
Janek0337 <artyleriapolska@gmail.com>
2026-04-11 00:07:08 +0200

    działająca wersja

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import numpy as np
 import cv2 as cv
 import keyboard
 import matricies
+from math import sin, cos
 
 class Camera:
     def __init__(self, camera_data):
         self.cx = camera_data[0]
         self.cy = camera_data[1]
         self.cz = camera_data[2]
-        self.kat_poziom = camera_data[3]
-        self.kat_pion = camera_data[4]
-        self.speed = 5
-        self.fov = 90
-        self.kat_speed = 15
+        self.kat_poziom = np.radians(camera_data[3])
+        self.kat_pion = np.radians(camera_data[4])
+        self.speed = 0.3
+        self.fov = np.radians(camera_data[5])
+        self.kat_speed = np.radians(10)
+        self.fov_speed = np.radians(5)
 
     def get_position(self):
         return (self.cx, self.cy, self.cz)
@@ -22,26 +24,42 @@ class Camera:
     
     def handle_input(self, event):
         if event.event_type == 'down':
+            fx = sin(self.kat_poziom)
+            fz = cos(self.kat_poziom)
+
+            rx = cos(self.kat_poziom)
+            rz = -sin(self.kat_poziom)
+
             if event.name == 'w':
-                self.cz += self.speed
+                self.cz += fz * self.speed
+                self.cx += fx * self.speed
             elif event.name == 's':
-                self.cz -= self.speed
+                self.cz -= fz * self.speed
+                self.cx -= fx * self.speed
             elif event.name == 'a':
-                self.cx -= self.speed
+                self.cx -= rx * self.speed
+                self.cz -= rz * self.speed
             elif event.name == 'd':
-                self.cx += self.speed
+                self.cx += rx * self.speed
+                self.cz += rz * self.speed
             elif event.name == 'q':
                 self.cy -= self.speed
             elif event.name == 'e':
                 self.cy += self.speed
             elif event.name == 'up':
-                self.kat_pion += self.kat_speed
-            elif event.name == 'down':
                 self.kat_pion -= self.kat_speed
+            elif event.name == 'down':
+                self.kat_pion += self.kat_speed
             elif event.name == 'right':
                 self.kat_poziom += self.kat_speed
             elif event.name == 'left':
                 self.kat_poziom -= self.kat_speed
+            elif event.name == 'z':
+                self.fov = max(np.radians(5), self.fov - self.fov_speed)
+                print(f"FOV: {np.rad2deg(self.fov)}")
+            elif event.name == 'x':
+                self.fov = min(np.radians(120), self.fov + self.fov_speed)
+                print(f"FOV: {np.rad2deg(self.fov)}")
 
 def read_input():
     all_points = []
@@ -54,6 +72,8 @@ def read_input():
         camera_pos = [float(x) for x in camera_pos]
         camera_pos[3] = camera_pos[3] % 360
         camera_pos[4] = camera_pos[4] % 360
+        camera = Camera(camera_pos)
+
         line = f.readline()
         while line:
             pts = line.strip().split(';')
@@ -67,7 +87,7 @@ def read_input():
             p2 = [float(x) for x in p2]
             all_points.append((p1, p2))
             line = f.readline()
-    return (Camera(camera_pos), all_points)
+    return (camera, all_points)
 
 def make_point_matrix(all_points: list[tuple[list[float], list[float]]]):
     matrix = np.array(all_points)
@@ -79,11 +99,16 @@ def make_point_matrix(all_points: list[tuple[list[float], list[float]]]):
     return points4d
 
 def main():
-    camera, all_pts = read_input()
-    
-    print(make_point_matrix(all_pts))
     width = 1000
     height = 600
+    img = np.full((height, width, 3), 255, dtype=np.uint8)
+    color = (0,0,0)
+
+    camera, all_pts = read_input()
+    base_pts = make_point_matrix(all_pts)
+
+    all_pts = matricies.transform_to_camera(base_pts, camera.get_position(), camera.get_angle())
+    points_on_plain = matricies.points_on_plain(all_pts, camera, width, height)
 
     keyboard.hook(camera.handle_input)
     while True:
@@ -92,35 +117,20 @@ def main():
         if keyboard.is_pressed('alt'):
             print(f'Pozycja kamery: {camera.get_position()}; {camera.get_angle()}')
 
-def transformacja_wzgledem_kamery(punkty: np.ndarray, pozycja_kamery: tuple[float, float, float], kat_poziom: float, kat_pion: float):
-    cx, cy, cz = pozycja_kamery
-    stranslatowane = matricies.translacja(punkty, -cx, -cy, -cz)
-    
-    kat_poziom_rad = np.radians(-kat_poziom)
-    kat_pion_rad = np.radians(-kat_pion)
+        transformed_pts = matricies.transform_to_camera(base_pts, camera.get_position(), camera.get_angle())
+        points_on_plain = matricies.points_on_plain(transformed_pts, camera, width, height)
 
-    punkty_o_Y = matricies.obrot_wokol_Y(stranslatowane, kat_poziom_rad)
-    punkty_o_X = matricies.obrot_wokol_X(punkty_o_Y, kat_pion_rad)
-
-    return punkty_o_X
-
-"""
-color = (255,255,255)
-    img = np.full((width ,height, 3), 255, dtype=np.uint8)
-    cv.line(img, (256, -50), (-50, 256), color=color, thickness=2)
-    cv.imshow("Usmiechnij sie, jestes w ukrytej kamerze", img)
-    keyboard.hook(on_key_event)
-    while True:
-        i += 1
-        i %= 256
-        color = (i % 256,(i - 67) % 256,(2*i) % 256)
-        cv.line(img, (256, -50), (-50, 256), color=color, thickness=2)
+        img = np.full((height, width, 3), 255, dtype=np.uint8)
+        for i in range(0, len(points_on_plain), 2):
+            x1, y1 = points_on_plain[i, :]
+            x2, y2 = points_on_plain[i+1, :]
+            cv.line(img, (int(round(x1)), int(round(y1))), (int(round(x2)), int(round(y2))), color=color, thickness=2)
         cv.imshow("Usmiechnij sie, jestes w ukrytej kamerze", img)
-        if cv.waitKey(1) & 0xFF == 27:
+        if cv.waitKey(16) & 0xFF == 27:
             break
-        if keyboard.is_pressed('esc'):
-            break
+
     cv.destroyAllWindows()
-"""
+    keyboard.unhook_all()
+
 if __name__ == "__main__":
     main()
--- matricies.py
+++ matricies.py
@@ -1,5 +1,6 @@
 import numpy as np
-from math import sin, cos
+from math import sin, cos, tan
+from main import Camera
 
 def translacja(matrix: np.ndarray, Tx: float, Ty: float, Tz: float) -> np.ndarray:
     translation_matrix = np.array([[1,0,0,0],
@@ -21,3 +22,33 @@ def obrot_wokol_Y(matrix: np.ndarray, phi: float) -> np.ndarray:
                                [sin(phi),0,cos(phi),0],
                                [0,0,0,1]])
     return matrix @ rotation_matrix
+
+def points_on_plain(points: np.ndarray, camera: Camera, width: int, height: int):
+    scaling_coef = 1 / tan(camera.fov / 2)
+    display_proportion = width / height
+    projection_matrix = np.array([[scaling_coef/display_proportion, 0, 0, 0],
+                                 [0, scaling_coef, 0, 0],
+                                 [0, 0, 0, 1],
+                                 [0, 0, 0, 0]])
+    projected_points = points @ projection_matrix
+    w = projected_points[:, 3].reshape(-1,1)
+    w[w == 0] = 0.0001
+    normalized_xy = projected_points[:, :2] / w
+    
+    normalized_x = normalized_xy[:, 0]
+    normalized_y = normalized_xy[:, 1]
+
+    scaled_x = (normalized_x + 1) * 0.5 * width
+    scaled_y = (1 - normalized_y) * 0.5 * height
+
+    return np.column_stack((scaled_x, scaled_y))
+
+def transform_to_camera(punkty: np.ndarray, pozycja_kamery: tuple[float, float, float], katy: tuple[float, float]):
+    cx, cy, cz = pozycja_kamery
+    kat_poziom, kat_pion = katy
+    stranslatowane = translacja(punkty, -cx, -cy, -cz)
+
+    punkty_o_Y = obrot_wokol_Y(stranslatowane, -kat_poziom)
+    punkty_o_X = obrot_wokol_X(punkty_o_Y, -kat_pion)
+
+    return punkty_o_X
